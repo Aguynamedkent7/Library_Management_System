1cdc6dfcfdf3658059cf93dc99c269d38c146fe2
Hyuse-USTP <christianjohn.legaspi@1.ustp.edu.ph>
2025-05-10 10:48:47 +0800

    Proper QR Structure

--- LMS_APP/src/main/java/com/example/LibraryManagementSystem/AdminDashboard.java
+++ LMS_APP/src/main/java/com/example/LibraryManagementSystem/AdminDashboard.java
@@ -1,10 +1,7 @@
 package com.example.LibraryManagementSystem;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import com.example.LibraryManagementSystem.ManageBooksUI;
-import com.example.LibraryManagementSystem.ManageBooksFunction;
+
 
 public class AdminDashboard extends JFrame {
     private static final Color BACKGROUND_COLOR = new Color(0xaa, 0xaa, 0xaa);
