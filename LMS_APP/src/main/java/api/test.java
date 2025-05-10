9864319fb50ea2eb0e486a6bcbbedca5f7b512b3
seodowa <pagalan.theodoreustp@gmail.com>
2025-05-10 14:54:31 +0800

    added basic search function

--- LMS_APP/src/main/java/api/test.java
+++ LMS_APP/src/main/java/api/test.java
@@ -1,6 +1,11 @@
 package api;
 
+import models.Book;
+
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Set;
+
 
 public class test {
     public static void main(String[] args) {
@@ -15,30 +20,72 @@ public class test {
            if (!conn.isValid(10)) {
                throw new SQLException();
            }
-            // SAMPLE QUERY
-           Statement stmt = conn.createStatement();
-           ResultSet rs = stmt.executeQuery("SELECT * FROM books");
-           while (rs.next()) {
-               int id = rs.getInt("id");
-               String title = rs.getString("title");
-               String author = rs.getString("author");
-               String published_date = rs.getString("published_date");
-               String publisher = rs.getString("publisher");
-               String is_borrowed = rs.getString("is_borrowed");
 
-               System.out.println("Book ID:" + id);
-               System.out.println("Title: " + title);
-               System.out.println("Author: " + author);
-               System.out.println("Published Date: " + published_date);
-               System.out.println("Publisher: " + publisher);
-               System.out.println("Is Borrowed: " + is_borrowed);
+            // SAMPLE QUERY
+           ArrayList<Book> Books = searchBooks(conn, "Bl", "; DROP TABLE books; --");
+
+           if (Books != null) {
+               for (Book book : Books) {
+                   System.out.println("Book " + book.getId());
+                   System.out.println("Title: " + book.getTitle());
+                   System.out.println("Author: " + book.getAuthor());
+                   System.out.println("Genre/s: " + book.getGenre());
+                   System.out.println("Publisher: " + book.getPublisher());
+                   System.out.println("Published Date: " + book.getPublished_Date());
+                   System.out.println("Is Borrowed? " + book.isBorrowed());
+                   System.out.println();
+               }
+           } else {
+               System.out.println("Empty search results.");
            }
-           rs.close();
-           stmt.close();
-           conn.close();
+
        } catch (SQLException e) {
-           System.out.println("Failed to connect to database: " + e);
+           System.out.println(e.getMessage());
        }
 
+    }
+
+    public static ArrayList<Book> searchBooks(Connection Conn, String SearchQuery, String SearchBy) {
+        ArrayList<Book> BookResults = new ArrayList<>();
+        try {
+            final Set<String> VALID_COLUMNS = Set.of(
+                    "title", "author", "genre", "publisher", "published_date"
+            );
+            if (!VALID_COLUMNS.contains(SearchBy)) {
+                throw new IllegalArgumentException("Invalid column: " + SearchBy);
+            }
+            String query = "SELECT " +
+                    "  books.id, books.title, books.author, string_agg(genres.genre_name, ', ') AS genres," +
+                    "  publisher_name, published_date, is_borrowed " +
+                    "FROM books " +
+                    "INNER JOIN genres_of_book ON books.id = genres_of_book.book_id " +
+                    "INNER JOIN genres ON genres_of_book.genre_id = genres.id " +
+                    "INNER JOIN publishers ON publishers.id = books.publisher_id " +
+                    "WHERE " + SearchBy + " LIKE ? " +
+                    "GROUP BY books.id, books.title, publisher_name, published_date";
+            PreparedStatement pstmt = Conn.prepareStatement(query);
+//            pstmt.setString(1, SearchBy);
+            pstmt.setString(1, '%' + SearchQuery + '%');
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                Book book = new Book (
+                        rs.getInt("id"),
+                        rs.getString("title"),
+                        rs.getString("author"),
+                        rs.getString("genres"),
+                        rs.getString("publisher_name"),
+                        rs.getString("published_date"),
+                        rs.getBoolean("is_borrowed")
+                );
+                BookResults.add(book);
+            }
+            rs.close();
+            pstmt.close();
+        } catch (SQLException | IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+        return BookResults;
     }
 }
---
+++ LMS_APP/src/main/java/models/Book.java
@@ -0,0 +1,78 @@
+package models;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private String genre;
+    private String publisher;
+    private String published_date;
+    private boolean is_borrowed;
+
+    public Book (int id, String title, String author, String genre, String publisher, String published_date, boolean is_borrowed) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.genre = genre;
+        this.publisher = publisher;
+        this.published_date = published_date;
+        this.is_borrowed = is_borrowed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPublished_Date() {
+        return published_date;
+    }
+
+    public void setPublished_Date(String published_date) {
+        this.published_date = published_date;
+    }
+
+    public boolean isBorrowed() {
+        return is_borrowed;
+    }
+
+    public void setIsBorrowed(boolean is_borrowed) {
+        this.is_borrowed = is_borrowed;
+    }
+
+}
