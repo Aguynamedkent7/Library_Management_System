0c06fc2dbd8fb5cf20cb96416f32b400ebf541b1
Hyuse-USTP <christianjohn.legaspi@1.ustp.edu.ph>
2025-05-16 10:08:19 +0800

    LibraryManagementSystemApp.java is now main class

--- LMS_APP/src/main/java/com/example/LibraryManagementSystem/LibraryManagementSystemApp.java
+++ LMS_APP/src/main/java/com/example/LibraryManagementSystem/LibraryManagementSystemApp.java
@@ -1,13 +1,12 @@
 package com.example.LibraryManagementSystem;
-import javax.swing.SwingUtilities;
 
-public class   LibraryManagementSystemApp {
+import javax.swing.*;
+
+public class LibraryManagementSystemApp {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            ManageBooksUI view = new ManageBooksUI();
-            ManageBooksFunction controller = new ManageBooksFunction(view);
-            view.setController(controller);
-            view.show();
+            AdminDashboard dashboard = new AdminDashboard();
+            dashboard.setVisible(true);
         });
     }
 }
